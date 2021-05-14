ef43d8f018282ab0113467599be4cc4481678275
sang4196 <sang4196@naver.com>
2021-05-14 14:13:16 +0900

    aaaaaa

---
+++ WPF_Connect6/WPF_Connect6/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Connect6.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+
+        #region notifyproperty
+        public event PropertyChangedEventHandler PropertyChanged;
+        //private void NotifyPropertyChanged(string propertyName)
+        //{
+        //    if(this.PropertyChanged != null)
+        //    {
+        //        this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        //    }
+        //}
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
