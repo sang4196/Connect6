2197e12faacaacdd30284cf46c57748cf4af4b1a
sangho lee <sang4196@naver.com>
2021-11-23 09:56:09 +0900

    start

--- WPF_Connect6/WPF_Connect6/obj/Debug/net5.0-windows/App.g.i.cs
+++ WPF_Connect6/WPF_Connect6/obj/Debug/net5.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace WPF_Connect6 {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.11.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace WPF_Connect6 {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.11.0")]
         public static void Main() {
             WPF_Connect6.App app = new WPF_Connect6.App();
             app.InitializeComponent();
